075e13260136ccd5bb4bcd48e84c382298504846
Thendo Ramashia <kramashia101@gmail.com>
2024-03-04 22:03:56 +0200

    Wrote unit test to check for initialized bean

--- src/test/java/com/springboot/blog/SpringbootBlogAppApplicationTests.java
+++ src/test/java/com/springboot/blog/SpringbootBlogAppApplicationTests.java
@@ -1,13 +1,20 @@
 package com.springboot.blog;
 
+import com.springboot.blog.controller.PostRestController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootBlogAppApplicationTests {
 
+	@Autowired
+	PostRestController postRestController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(postRestController).isNotNull();
 	}
 
 }
